75c67a057a1b8a6d03d84d0956c3393f5dfbe28c
LukeLehane <lukelehane@live.com>
2016-02-07 16:12:32 +0000

    added 'FizzBuzz' functionality

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -3,10 +3,15 @@
 var number = 1;
 
 while (number <= 100) {
-	if (number % 3 == 0) {
+	if (number % 3 == 0 && number % 5 != 0) {
 		console.log("Fizz")
 	} else if (number % 5 == 0 && number % 3 != 0) {
 		console.log("Buzz")
+
+	} else if (number % 3 == 0 && number % 5 ==0) {
+
+		console.log("FizzBuzz")
+
 	} else {
 
 		console.log(number);
