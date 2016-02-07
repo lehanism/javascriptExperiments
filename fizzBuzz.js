849882f76230cb0a260fe590dbd456e9b115da37
LukeLehane <lukelehane@live.com>
2016-02-07 12:51:33 +0000

    did a fizz buzzzzzzzz

---
+++ fizzBuzz.js
@@ -0,0 +1,16 @@
+//Print 1 to 100. For numbers divisible by 3, print "Fizz", instead of number, and for numbers divisible by 5 (and not 3), print 'Buzz' instead.
+
+var number = 1;
+
+while (number <= 100) {
+	if (number % 3 == 0) {
+		console.log("Fizz")
+	} else if (number % 5 == 0 && number % 3 != 0) {
+		console.log("Buzz")
+	} else {
+
+		console.log(number);
+	}
+
+	number++;
+}
