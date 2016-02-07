9293dcbc81d334663dd9ba88f76bb92eb196f272
LukeLehane <lukelehane@live.com>
2016-02-07 12:23:15 +0000

    add first js attempt

---
+++ loopingATriangle.js
@@ -0,0 +1,9 @@
+//write a loop that makes seven calls to console.log to output a triangle of hashtags
+
+var hashtags = "#";
+
+while (hashtags.length <= 7) {
+	console.log(hashtags)
+	hashtags = hashtags + "#";
+}
+
